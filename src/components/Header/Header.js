ef228106b5a94251c42c8a0a1eecc1730ca3cfa5
Ashutosh Giri <ashutoshgiri249@gmail.com>
2023-07-28 11:27:52 +0530

    added the responsive nav bar and responsive product-detail

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,21 +6,37 @@ import SearchBar from '../SearchBar/SearchBar'
 
 const Header = () => {
   return (
-    <div className='nav-container'>
-        <div className='logo-container'>
-            <img src={Logo} alt="logo"/>
-        </div>
-        <SearchBar/>
-        <div className='nav-items'>
-            <ul>
-                <Link to ="/" className='nav-link'><li>Home</li></Link>
-                <li>Contact Us</li>
-                <li>About Us</li>
-                <li>Cart</li>
-                <li>Sign In</li>
-            </ul>
-        </div>
-    </div>
+    <nav>
+      <input type="checkbox" id="check" />
+      <label htmlFor="check" className='checkBtn'>
+        Menu
+        <i className ='fa fa-bars'></i>
+      </label>
+      <label className='logo'>Lets Shop!</label>
+      
+      <ul>
+        <Link to = "/" className='nav-link'><li>Home</li></Link>
+        <Link to = "/" className='nav-link'><li>Contact Us</li></Link>
+        <Link to = "/" className='nav-link'><li>About Us</li></Link>
+        <Link to = "/" className='nav-link'><li>Sign In</li></Link>
+        <Link to = "/" className='nav-link'><li>Cart</li></Link>
+      </ul>
+    </nav>
+    // <div className='nav-container'>
+    //     <div className='logo-container'>
+    //         <img src={Logo} alt="logo"/>
+    //     </div>
+    //     <SearchBar/>
+    //     <div className='nav-items'>
+    //         <ul>
+    //             <Link to ="/" className='nav-link'><li>Home</li></Link>
+    //             <li>Contact Us</li>
+    //             <li>About Us</li>
+    //             <li>Cart</li>
+    //             <li>Sign In</li>
+    //         </ul>
+    //     </div>
+    // </div>
   )
 }
 
--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -8,11 +8,11 @@ import Shimmer from "../ShimmerUI/Shimmer";
 import DetailShimmer from "../ShimmerUI/DetailShimmer";
 
 const ProductDetail = (props) => {
-  const [isLoading,setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
   const { id, title, image, price, description, category } = props.product;
-  const {data} = useContext(ProductContext);
+  const { data } = useContext(ProductContext);
   const [similarProducts, setSimilarProducts] = useState([]);
-  
+
   const filterProducts = () => {
     const filterList = data.filter((product) => product.category === category);
     const updateProductList = filterList.filter((product) => product.id !== id)
@@ -21,42 +21,44 @@ const ProductDetail = (props) => {
   }
 
   useEffect(() => {
-    
-    const delay = 1000; 
+
+    const delay = 1000;
     setTimeout(() => {
       filterProducts();
       setIsLoading(false);
     }, delay);
-    
-  },[])
-  if (isLoading) return <><DetailShimmer/><Shimmer/></>
+
+  }, [])
+  if (isLoading) return <><DetailShimmer /><Shimmer /></>
   return (
     <>
-  
-    <div className="product-detail">
-      <div className="product-image">
-        <img src={image} alt="image" />
+      <div className="prod-container">
+
+        <div className="product-detail">
+          <div className="product-image">
+            <img src={image} alt="image" />
+          </div>
+          <div className="product-info">
+            <h1 className="product-title">{title}</h1>
+            <p className="product-description">Description - {description}</p>
+            <p className="product-price">Rs {price}/-</p>
+            <button className="detail-add-to-cart-btn">Add to Cart</button>
+          </div>
+
+        </div>
       </div>
-      <div className="product-info">
-        <h1 className="product-title">{title}</h1>
-        <p className="product-description">Description - {description}</p>
-        <p className="product-price">Rs {price}/-</p>
-        <button className="detail-add-to-cart-btn">Add to Cart</button>
+      <div className="similar-prod-container">
+        <h2 className="heading">Similar Products</h2>
+        <div className="prod-card-container">
+          {
+            similarProducts.map((product) => (
+              <Link to={"/productdetail/" + product.id} key={product.id} className='product-link'>
+                <ProductCard id={product.id} title={product.title} image={product.image} price={product.price} />
+              </Link>
+            ))
+          }
+        </div>
       </div>
-      
-    </div>
-    <div className="similar-prod-container">
-      <h2 className="heading">Similar Products</h2>
-      <div className="prod-card-container">
-      {
-        similarProducts.map((product) => (
-          <Link to={"/productdetail/" + product.id} key={product.id} className='product-link'>
-            <ProductCard id={product.id} title={product.title} image={product.image} price={product.price} />
-          </Link>
-        ))
-      }
-      </div>
-    </div>
     </>
   );
 };
